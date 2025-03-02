0bef002cccf8e62b33c3da7077b2f4946767969a
vouks32 <85527577+vouks32@users.noreply.github.com>
2025-03-02 19:26:21 +0100

    Update index.js - 2

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const port = 7872;  //Save the port number where your server will be listening
 
 app.use(cors({
   allowedHeaders: "*",
-  origin: "*",
+  origin: function (origin, callback) { // allow requests with no origin  // (like mobile apps or curl requests)
+        return callback(null, true);
+    }
   methods: ["GET", "POST"]
 }));
 
